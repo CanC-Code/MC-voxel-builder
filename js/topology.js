bcb2672f064d805dd110deb9ea9338fb0030f9f7
CanC-Code <cardscollectiblesandoddities@gmail.com>
2025-12-18 17:44:21 -0700

    Update topology.js

--- js/topology.js
+++ js/topology.js
@@ -1,11 +1,3 @@
 export function ensureTopology(geometry) {
   geometry.computeVertexNormals();
-  geometry.computeBoundingBox();
-  geometry.computeBoundingSphere();
-
-  if (!geometry.attributes.normal) {
-    geometry.computeVertexNormals();
-  }
-
-  geometry.attributes.position.setUsage(35048); // DynamicDrawUsage
 }
